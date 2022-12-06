d64ef889c3aede7333c14297466fa57d6f7e1e24
Valeria <valeria89_07@mail.ru>
2022-12-06 12:38:42 +0300

    задача8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,11 @@
+﻿// число N и на выходе показывает все четныемчисла от 1 до N
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int A = 1;
+
+while (A <= N)
+{
+    if (A%2 == 0)
+    Console.WriteLine($"{A} ");
+    A++;
+}
